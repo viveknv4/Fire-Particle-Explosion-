731257abab510c9e095487804a2a89a63082f3f2
vivek <viveknv4@gmail.com>
2016-02-06 23:02:21 +0530

    Generate Random Particles

--- FireParticleExplosion.cpp
+++ FireParticleExplosion.cpp
@@ -9,16 +9,22 @@
 #include <iostream>
 #include <SDL.h>
 #include "Screen.h"
+#include <stdlib.h>
+#include <time.h>
+#include "Swarm.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 
+	srand(time(NULL));
 	Screen screen(800, 600);
 	if(screen.init() == false){
 		cout << "Couldn't Initialize SDL "<<endl;
 		return -1;
 	}
+
+	Swarm swarm;
 
 	//Game_Loop--------------------------------------
 	while(true){
@@ -28,13 +34,23 @@ int main(int argc, char *argv[]){
 		int red = ((1+sin(elapsed * 0.0002)) * 128);
 		int blue = ((1+sin(elapsed * 0.0003)) * 128);
 
+		Particle* pParticles = swarm.getParticles();
+		for(int i=0; i<swarm.NPARTICLES; i++){
+			Particle points = pParticles[i];
+			int x = (points.m_x+1) * (screen.SCREEN_WIDTH/2);
+			int y = (points.m_y+1) * (screen.SCREEN_HEIGHT/2);
+
+			screen.setPixel(x,y,red,green,blue);
+		}
+
+		/*
 		for(int x=0; x<screen.SCREEN_WIDTH; x++){
 			for(int y=0; y<screen.SCREEN_HEIGHT; y++){
 
 				screen.setPixel(x,y,red,green,blue);
 			}
-		}
-		screen.setPixel(400, 300, 255, 255, 255);
+		}*/
+
 		screen.update();
 		if(screen.processEvents() == false)
 			break;
